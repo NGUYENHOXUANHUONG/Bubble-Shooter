dc9c85d895026f4f1047272a2c90cc50183f9743
NGUYENHOXUANHUONG <xuanhuong2002.hp@gmail.com>
2023-06-10 17:20:53 +0700

    add2

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -1,6 +1,8 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -14,53 +16,182 @@ public class BallSpawner : MonoBehaviour
     [SerializeField] bool useRay;
     [SerializeField] bool useLine;
     [SerializeField] LineRenderer line;
+    List<Vector3> rayList = new List<Vector3>();
+
+    public GameObject ball;
+    public float ballSpeed = 10f;
+    private bool isBallMoving = true;
+    private Vector3 nextPos;
 
     public void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        //Debug.Log(isBallMoving);
+
+        if (!isBallMoving) return;
+        Debug.Log("====" + isBallMoving);
+        rayList = new List<Vector3>();
+
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 dir = (pos - transform.position).normalized;
+        rayList.Add(transform.position);
+        _ray = Physics2D.Raycast(transform.position, dir, 20f);
+
+        _angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+
+        if (_angle >= minMaxAngle.x && _angle <= minMaxAngle.y)
         {
-            _ray = Physics2D.Raycast(transform.position, transform.up, 20f, _layMask);
-            
-            Vector2 recflactPos = Vector2.Reflect(new Vector3(_ray.point.x, _ray.point.y) - transform.position, _ray.normal);
-            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-            Vector3 dir = Input.mousePosition - pos;
-
-            _angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
-
-            if (_angle >= minMaxAngle.x && _angle <= minMaxAngle.y)
+            if (_ray.collider.tag == "top")
             {
-
-                if (useRay)
-                {
-                    Debug.DrawRay(transform.position, transform.up * _ray.distance * 2f, Color.green);
-                    Debug.DrawRay(_ray.point, recflactPos.normalized * 4, Color.green);
-                }
-
-                if (useLine)
-                {
-                    if (_ray.collider.tag == "top")
-                    {
-                        line.positionCount = 2;
-                        line.SetPosition(0, transform.position);
-                        line.SetPosition(1, _ray.point);
-                    }
-                    else
-                    {
-                        line.positionCount = 3;
-                        line.SetPosition(0, transform.position);
-                        line.SetPosition(1, _ray.point);
-                        //Debug.Log("===" + _ray.point);
-                        line.SetPosition(2, _ray.point + recflactPos.normalized * 2f);
-                        //Debug.Log("========" + _ray.point + recflactPos.normalized * 2f);
-                    }
-
-                }
+                line.positionCount = 2;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, _ray.point);
+                rayList.Add(_ray.point);
             }
-            transform.rotation = Quaternion.AngleAxis(_angle, transform.forward);
-                
+            if (_ray.collider.tag == "Boundary")
+            {
+                rayList.Add(_ray.point);
+                Vector2 recflactPos = Vector2.Reflect(dir, Vector2.right);
+
+                if (_ray.collider.name == "right")
+                {
+                    Vector2 moveleft = (Vector2)_ray.point - new Vector2(0.01f, 0);
+                    _ray = Physics2D.Raycast(moveleft, recflactPos, 20f);
+                }
+                if (_ray.collider.name == "left")
+                {
+                    Vector2 moveright = (Vector2)_ray.point + new Vector2(0.01f, 0);
+                    _ray = Physics2D.Raycast(moveright, recflactPos, 20f);
+                }
+
+                rayList.Add(_ray.point);
+                line.positionCount = 3;
+                line.SetPosition(0, rayList[0]);
+                line.SetPosition(1, rayList[1]);
+                line.SetPosition(2, rayList[2]);
+
+            }
+            //if (Input.GetMouseButtonDown(0))
+            //{
+            //    BallMove();
+            //}
+
         }
-        
+
+        transform.rotation = Quaternion.AngleAxis(_angle, transform.forward);
     }
 
+
+    void BallMove()
+    {
+        isBallMoving = false;
+        List<Vector3> iList = rayList;
+
+        GameObject obj = Instantiate(ball);
+        obj.transform.position = transform.position;
+
+        if (iList.Count < 3)
+        {
+            obj.transform.DOMove(iList[1], 1f).OnComplete(() =>
+            {
+                isBallMoving = true;
+            });
+        }
+        else
+        {
+            obj.transform.DOMove(iList[1], 1f).OnComplete(() =>
+            {
+                obj.transform.DOMove(iList[2], 1f).OnComplete(() =>
+                {
+                    isBallMoving = true;
+                });
+            });
+        }
+    }
+    void MoveBall()
+    {
+        bool isMovenext = false;
+
+        if (!isBallMoving)
+        {
+            return;
+        }
+        List<Vector3> moveList = rayList;
+
+        ball.transform.position = Vector3.MoveTowards(ball.transform.position, nextPos, ballSpeed * Time.deltaTime);
+
+        if (moveList.Count < 3)
+        {
+            Debug.Log(moveList[0] + "====" + moveList.Count);
+
+            if (ball.transform.position == moveList[1])
+            {
+                isBallMoving = false;
+            }
+        }
+        else
+        {
+            if (ball.transform.position == moveList[2])
+            {
+                isBallMoving = false;
+            }
+            if (ball.transform.position == moveList[1])
+            {
+                if (isMovenext)
+                {
+                    return;
+                }
+                nextPos = rayList[2];
+                isMovenext = true;
+            }
+        }
+
+
+    }
+
+    void RayCastHit()
+    {
+        rayList = new List<Vector3>();
+
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 dir = (pos - transform.position).normalized;
+        rayList.Add(transform.position);
+        _ray = Physics2D.Raycast(transform.position, dir, 20f);
+
+        _angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+
+        if (_angle >= minMaxAngle.x && _angle <= minMaxAngle.y)
+        {
+            if (_ray.collider.tag == "top")
+            {
+                line.positionCount = 2;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, _ray.point);
+                rayList.Add(_ray.point);
+            }
+            if (_ray.collider.tag == "Boundary")
+            {
+                rayList.Add(_ray.point);
+                Vector2 recflactPos = Vector2.Reflect(dir, Vector2.right);
+
+                if (_ray.collider.name == "right")
+                {
+                    Vector2 moveleft = (Vector2)_ray.point - new Vector2(0.01f, 0);
+                    _ray = Physics2D.Raycast(moveleft, recflactPos, 20f);
+                }
+                if (_ray.collider.name == "left")
+                {
+                    Vector2 moveright = (Vector2)_ray.point + new Vector2(0.01f, 0);
+                    _ray = Physics2D.Raycast(moveright, recflactPos, 20f);
+                }
+
+                rayList.Add(_ray.point);
+                line.positionCount = 3;
+                line.SetPosition(0, rayList[0]);
+                line.SetPosition(1, rayList[1]);
+                line.SetPosition(2, rayList[2]);
+
+            }
+        }
+    }
 
 }
